86fddde2ad83d7b6599bc1151a4246a4d2b6c5b9
Chloe ONeil <chloesporri@yahoo.com>
2022-10-26 20:26:46 -0700

    add README.md and adjusted window.alert to add string

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,4 @@ function add(number1, number2) {
 const number1=parseInt(prompt("Enter a number:"));
 const number2=parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The addition of your numbers equal "+ (add(number1, number2))+ ".");
