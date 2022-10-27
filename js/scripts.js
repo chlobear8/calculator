3ab260a748664860c18e1158ad99950b8a74d094
Chloe ONeil <chloesporri@yahoo.com>
2022-10-27 09:41:00 -0700

    add window.alert for all functions

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,22 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-//user interface logic
-const number1=parseInt(prompt("Enter a number:"));
-const number2=parseInt(prompt("Enter another number:"));
+function subtract(number1, number2) {
+  return number1 - number2;
+}
 
-window.alert("The addition of your numbers equal "+ (add(number1, number2))+ ".");
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+
+//user interface logic
+let number1=parseInt(prompt("Enter a number:"));
+let number2=parseInt(prompt("Enter another number:"));
+
+window.alert("The addition of your numbers equal "+ (add(number1, number2))+ ".")"\n"+("The subtraction of your numbers equal "+ (subtract(number1, number2))+ ".")"\n"+("The multiplication of your numbers equal "+ (multiply(number1, number2))+ ".")"\n"+("The division of your numbers equal "+(divide(number1, number2))+ ".");
+
